bf8097ac5dae8191db3bd41ce2c9a485a6a14474
manuelapilongo <rullionmadev@gmail.com>
2015-10-24 22:11:58 +0000

    finally

--- code/register/load.js
+++ code/register/load.js
@@ -9,9 +9,13 @@ module.exports = function(params)
         user  = {user_name: name, phone_number: phone };
 
     conn.query("INSERT INTO users SET ?", user, function (err, resp) {
-      if (err) { console.log(err); res.send("PROBLEM"); return;}
-      
+      if (err) {
+        console.log(err); res.send("PROBLEM");
+        return;
+      }
+
       sms.sendSMS(phone, "Thank you for registering");
+
       res.send("Thank you. We've send you confirmation to your phone number")
     });
 
