77b11dd77832b6e082482a3a8332aac170d679e2
jkrank <krankjoseph@gmail.com>
2015-10-24 15:53:58 +0000

    .

--- code/main.js
+++ code/main.js
@@ -5,20 +5,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-<<<<<<< HEAD
-app.get('/routes', require('./routes').index);
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/soundex', function (req, res) {
-  res.send(req.query.word);
-  res.sen
-});
-
-=======
->>>>>>> 24bb1cd8ae9bcb1c900a4fd99d80165c1aaa6ac3
 app.use('/static', express.static(__dirname + '/public'));
 
 require('./sign_in/load.js')({app: app});
