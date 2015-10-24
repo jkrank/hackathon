4f08951f524e7ee158705e0d21f185e766c1babd
manuelapilongo <rullionmadev@gmail.com>
2015-10-24 14:38:33 +0000

    templating

--- code/sign_in/load.js
+++ code/sign_in/load.js
@@ -1,7 +1,5 @@
 module.exports = function(params)
 {
   var app = params.app;
-    app.get('/sign_in', function (req, res) {
-      res.send("testing the sing_in page");
-    });
+  app.get('/sign_in', require('../routes/sign_in').sign_in);
 }
