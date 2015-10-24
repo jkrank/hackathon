2e17e57c60a5861ae06d6c9c8c4a4a1acecba096
Mark Greenwood <greenwma@googlemail.com>
2015-10-24 14:33:54 +0000

    Updated sms

--- code/sms/sms.js
+++ code/sms/sms.js
@@ -1,13 +1,14 @@
 module.exports = function(params)
 {
-  var app = params.app;
+  var app = params.app,
+      api_key = require("./clockwork_api_key.js");
     app.get('/receive-sms', function (req, res) {
       var to     = req.query.to,
           from   = req.query.from,
           text   = req.query.content,
           msg_id = req.query.msg_id;
 
-      res.send("from: "+from+"<br>to"+to+"<br>content: "+text)
+      res.send("api_key: "+api_key+"<br>from: "+from+"<br>to"+to+"<br>content: "+text)
   });
 
   sendSMS = function(to, text) {
