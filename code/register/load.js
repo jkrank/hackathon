c245bb4a0735ca99244f58f21831aa95f4beeb7d
jkrank <krankjoseph@gmail.com>
2015-10-24 23:21:47 +0000

    .

--- code/register/load.js
+++ code/register/load.js
@@ -3,19 +3,19 @@ module.exports = function(params)
   var app  = params.app,
       conn = params.conn,
       sms  = params.sms;
-  app.post('/register', function (req, res) {
-    var name = req.body.name,
-        phone = req.body.phone,
+  app.post('/register', function (reg, res) {
+    var name = req.query.name,
+        phone = req.query.phone,
         user  = {user_name: name, phone_number: phone };
 
     conn.query("INSERT INTO users SET ?", user, function (err, resp) {
       if (err) {
         console.log(err);
-        res.send("PROBLEM");
+        res.render('../views/sign_in_failed.jsx', { title: 'Sign In Failed' });
         return;
       }
 
-      sms.sendSMS(phone, "Welcome to BrokePhone! Reply to this message with your phrase");
+      sms.sendSMS(phone, "Thank you for registering");
 
       res.send("Thank you. We've send you confirmation to your phone number")
     });
