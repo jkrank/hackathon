89b008e386a21653f760d41beb88cf0357f4f001
manuelapilongo <rullionmadev@gmail.com>
2015-10-25 05:29:00 +0000

    failed and success

--- code/public/scripts/sign_up_failed_components.js
+++ code/public/scripts/sign_up_failed_components.js
@@ -6,14 +6,14 @@ var RegisterImg = React.createClass({
 
 ReactDOM.render(
   <div className="container-fluid">
-    <div className="col-md-6 col-sm-6 col-sm-offset-3  ">
+    <div className="col-md-6 col-sm-6 col-sm-offset-3">
       <RegisterImg />
       <div className="alert alert-danger" role="alert">
         <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
         <span className="sr-only">Error:</span>
-        Failure during registration
+        Failure during registration {page.lib.sign_up_errors[window.location.toString().split("err=")[1]]}
       </div>
-      <a href="register" className='btn btn-danger'>Register</a>
+      <a href="/sign_up" className='btn btn-danger'>Register</a>
     </div>
   </div>
 , register_form_container);
--- code/register/load.js
+++ code/register/load.js
@@ -9,28 +9,28 @@ module.exports = function(params)
         user  = {user_name: name, phone_number: phone };
 
     if (!new RegExp("^\\d+$").test(phone.replace(/\D/g, '').replace(' ', ''))) {
-       res.render('../views/sign_up_failed.jsx', { name: name, title: 'Sign Up Failed' });
+       res.redirect(301, '/sign_up_failed?err=1');
        return;
     }
 
     conn.query("SELECT id from users WHERE phone_number = ?", [phone], function (err, result) {
         if (err) {
-            console.log(err);
-            res.render('../views/sign_up_failed.jsx', { name: name, title: 'Error while validating phone number' });
-            return;
+           console.log(err);
+           res.redirect(301, '/sign_up_failed?err=2');
+           return;
         }
         if (result.length > 0) {
-          res.send("This phone number is already registered");
+          res.redirect(301, '/sign_up_failed?err=3');
           return;
         }
         conn.query("INSERT INTO users SET ?", user, function (err, resp) {
           if (err) {
             console.log(err);
-            res.render('../views/sign_up_failed.jsx', { name: name, title: 'Sign Up Failed' });
+            res.redirect(301, '/sign_up_failed?err=4');
             return;
           }
           sms.sendSMS(phone, "Welcome to BrokePhone! Reply to this message with your phrase");
-          res.render('../views/sign_up_success.jsx', { name: name, title: 'Sign Up Successfull' });
+          res.redirect(301, '/sign_up_success');
         });
     });
   });
